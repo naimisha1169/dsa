cbf7167d9bc826196b98c1b965a414bb3135aaa2
naimisha1169 <naimisha.p6911@gmail.com>
2026-02-20 11:10:07 +0530

    Time: 8 ms (71.64%), Space: 48 MB (11.88%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,16 +1,17 @@
 class NumArray {
-private int[] num;
+    int[] num;
     public NumArray(int[] nums) {
+        num=new int[nums.length];
+        num[0]=nums[0];
         for(int i=1;i<nums.length;i++){
-            nums[i]+=nums[i-1];
+        num[i]=nums[i]+num[i-1];  
         }
-        this.num=nums;
     }
     
     public int sumRange(int left, int right) {
         if(left==0){
-        return num[right];
-    }
+            return num[right];
+        }
         return num[right]-num[left-1];
     }
 }
