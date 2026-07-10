0537e20101c8cdbcd54818cc4fe9da400fa3dcf3
naimisha1169 <naimisha.p6911@gmail.com>
2026-07-11 01:40:20 +0530

    Time: 1 ms (16.15%), Space: 44.5 MB (55.4%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int l=0,a=0;
+        while(l<accounts.length){
+            int r=0,c=0;
+            while(r<accounts[0].length){
+           c+=accounts[l][r];
+            a=Math.max(a,c);
+            r++;
+            }
+            l++;
+        }
+        return a;
+    }
+}
