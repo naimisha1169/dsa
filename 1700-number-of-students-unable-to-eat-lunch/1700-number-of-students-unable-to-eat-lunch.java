d2027fca1763abdfe3dab1501ba5560062a838af
naimisha1169 <naimisha.p6911@gmail.com>
2026-08-01 23:37:25 +0530

    Time: 0 ms (100%), Space: 42.9 MB (94%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -9,14 +9,14 @@ class Solution {
         }
         for (int sand : sandwiches) {
             if (sand == 0) {
-                if (c1 != 0) 
+                if (c1 != 0)
                     c1--;
-                 else
+                else
                     break;
             } else {
-                if (c2 != 0) 
+                if (c2 != 0)
                     c2--;
-                 else
+                else
                     break;
             }
         }
