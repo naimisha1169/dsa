1fe88d23e2067a45372310510e41c5cd78cbc9ac
naimisha1169 <naimisha.p6911@gmail.com>
2026-08-08 23:38:32 +0530

    Time: 4 ms (82.85%), Space: 46.7 MB (9.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,11 @@
 import java.util.*;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-      char[] c=s.toCharArray();
-      char[] b=t.toCharArray();
+        char[] c = s.toCharArray();
+        char[] b = t.toCharArray();
         Arrays.sort(c);
         Arrays.sort(b);
-        return Arrays.equals(b,c);
+        return Arrays.equals(b, c);
     }
 }
