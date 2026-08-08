c4d0da54d4536d91487cb2ee32b7a5b8c679c8d5
naimisha1169 <naimisha.p6911@gmail.com>
2026-08-08 23:37:28 +0530

    Time: 4 ms (82.85%), Space: 46.4 MB (34.98%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+      char[] c=s.toCharArray();
+      char[] b=t.toCharArray();
+        Arrays.sort(c);
+        Arrays.sort(b);
+        return Arrays.equals(b,c);
+    }
+}
