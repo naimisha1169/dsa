577cc44045134d7a78b3d6f44df67843ede4f772
naimisha1169 <naimisha.p6911@gmail.com>
2026-08-20 23:49:03 +0530

    Time: 2 ms (57.45%), Space: 46.7 MB (66.35%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -8,9 +8,7 @@ class Solution {
      list1.add(nums[0]);
      list2.add(nums[1]);
      for(int i=2;i<n;i++){
-        int last1=list1.get(list1.size()-1);
-        int last2=list2.get(list2.size()-1);
-        if(last1>last2){
+        if(list1.get(list1.size()-1)>list2.get(list2.size()-1)){
             list1.add(nums[i]);
         }
         else{
@@ -18,8 +16,8 @@ class Solution {
         }
      } 
      int a=0;
-     for(int i=0;i<list1.size();i++){
-        c[a++]=list1.get(i);
+     for(int i:list1){
+        c[a++]=i;
      }
      for(int i:list2){
         c[a++]=i;
