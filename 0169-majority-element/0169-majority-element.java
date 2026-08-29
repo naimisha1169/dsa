ea8af36d25d03c2fdb43a433eae65fdab78f463a
naimisha1169 <naimisha.p6911@gmail.com>
2026-08-29 23:59:04 +0530

    Time: 1 ms (99.89%), Space: 55.9 MB (6.68%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int c=0,vote=0;
+        for(int num:nums){
+            if(c==0)
+            vote=num;
+            if(vote==num)
+            c++;
+            else
+            c--;
+        }
+        return vote;
+    }
+}
