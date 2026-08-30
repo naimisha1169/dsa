86d62b1fcd8dc73cc45d1c0faf79f6bfbc7a5dbb
naimisha1169 <naimisha.p6911@gmail.com>
2026-08-30 23:33:26 +0530

    Time: 36 ms (25.09%), Space: 45.9 MB (73.72%) - LeetHub

---
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int c=0;
+        for(int i=low;i<=high;i++){
+            String s=Integer.toString(i);
+            int len=s.length();
+            if(len%2!=0) continue;
+            int mid=len/2;int l=0,r=0;
+            for(int j=0;j<mid;j++){
+                l+=s.charAt(j)-'0';
+                r+=s.charAt(j+mid)-'0';
+            }
+            if(l==r)
+            c++;
+        }
+        return c;
+    }
+}
