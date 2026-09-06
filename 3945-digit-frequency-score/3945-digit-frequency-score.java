7d107efb8abdab6c2777e42379a6e7e2468b18ad
naimisha1169 <naimisha.p6911@gmail.com>
2026-09-06 22:03:54 +0530

    Time: 1 ms (98.34%), Space: 42.5 MB (69.48%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,10 +1,10 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-       int sum=0;
+       int ans=0;
         while(n>0){
-            sum+=n%10;
+            ans+=n%10;
             n/=10;
         }
-        return sum;
+        return ans;
     }
 }
