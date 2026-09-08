ce05cc43a981c24675035ca486f0f5c0679ae5e1
naimisha1169 <naimisha.p6911@gmail.com>
2026-09-08 23:19:18 +0530

    Time: 8 ms (8.47%), Space: 42.6 MB (39.85%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countCommas(int n) {
+        int c=0;
+        for(int i=1;i<=n;i++){
+            if(i<1000)
+            continue;
+            else
+            c++;
+        }
+        return c;
+    }
+}
