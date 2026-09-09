23b209036696c4e4f7ab5f154ae9cdbf357c4103
naimisha1169 <naimisha.p6911@gmail.com>
2026-09-09 23:28:26 +0530

    Time: 1 ms (96.37%), Space: 46.2 MB (82.96%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.java
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int alternatingSum(int[] nums) {
-        int c = 0;
+        int sum = 0;
 
         for (int i = 0; i < nums.length; i++) {
             if (i % 2 == 0) {
-                c += nums[i];
+                sum += nums[i];
             } else {
-                c -= nums[i];
+                sum -= nums[i];
             }
         }
-        
-        return c;
+
+        return sum;
     }
 }
