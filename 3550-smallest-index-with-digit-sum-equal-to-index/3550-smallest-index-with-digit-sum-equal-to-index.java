a70446b1b4bbdbc7bdf0014a5f23546ba81ca9b9
naimisha1169 <naimisha.p6911@gmail.com>
2026-09-24 23:34:47 +0530

    Time: 1 ms (99.69%), Space: 45.8 MB (24.38%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,18 +1,14 @@
 class Solution {
     public int smallestIndex(int[] nums) {
-      int o=0;
        for(int i=0;i<nums.length;i++){
         int j=nums[i],sum=0;
         while(j>0){
             sum+=j%10;
             j/=10;
         }
-        if(sum==i){
-        o=i;
-        break;
+        if(sum==i)
+        return i;
        }
-        else o=-1;
-       }
-       return o;
+       return -1;
     }
 }
