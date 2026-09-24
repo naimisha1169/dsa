91b3bd229076cb2f0ba0adef24ac26fd33de06c2
naimisha1169 <naimisha.p6911@gmail.com>
2026-09-24 23:35:13 +0530

    Time: 1 ms (99.69%), Space: 45.8 MB (11.42%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -3,7 +3,8 @@ class Solution {
        for(int i=0;i<nums.length;i++){
         int j=nums[i],sum=0;
         while(j>0){
-            sum+=j%10;
+           int a=j%10;
+            sum+=a;
             j/=10;
         }
         if(sum==i)
